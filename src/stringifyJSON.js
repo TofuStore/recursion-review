4106a99e7fc7c75403143164ba92735970d67745
Karli Thomas <karliholthouse@gmail.com>
2022-01-03 14:38:28 -0800

    Completed stringifyJSON implementation

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,43 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj;
+  }
+
+  if (!obj) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    var result = [];
+
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+
+    result = result.join(',');
+
+    return '[' + result + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var result = [];
+
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        var newKey = stringifyJSON(key);
+        var value = stringifyJSON(obj[key]);
+        var insert = newKey + ':' + value;
+        result.push(insert);
+      }
+    }
+
+    result = result.join(',');
+    return '{' + result + '}';
+  }
 };
